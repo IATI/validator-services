0d12c516b13028aab65c13c5078065231ff63b5d
Alex Lydiate <alexl@devinit.org>
2021-04-27 15:05:20 +0100

    changed path, commented out try/catch for the moment to see errors

--- pub-get-report/index.js
+++ pub-get-report/index.js
@@ -19,46 +19,46 @@ module.exports = async (context, req) => {
 
         return;
     }
-    try {
-        let result = null;
+    // try {
+    let result = null;
 
-        if (id) {
-            result = await db.getReportForId(id);
-        } else if (hash) {
-            result = await db.getReportForHash(hash);
-        } else if (url) {
-            result = await db.getReportForUrl(url);
-        }
+    if (id) {
+        result = await db.getReportForId(id);
+    } else if (hash) {
+        result = await db.getReportForHash(hash);
+    } else if (url) {
+        result = await db.getReportForUrl(url);
+    }
 
-        if (result === null) {
-            const message = { client_error: 'The requested report does not exist.' };
-
-            context.res = {
-                status: 404,
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(message),
-            };
-
-            return;
-        }
+    if (result === null) {
+        const message = { client_error: 'The requested report does not exist.' };
 
         context.res = {
-            status: 200,
+            status: 404,
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(result),
+            body: JSON.stringify(message),
         };
 
         return;
-    } catch (e) {
+    }
+
+    context.res = {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(result),
+    };
+
+    
+    /* } catch (e) {
         const message = {
             server_error:
                 "Apologies - there's been an unexpected error on our side. Please contact support@iatistandard.org if you need help, quoting the full request you made.",
         };
-
+ 
         context.res = {
             status: 500,
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(message),
         };
-    }
+    } */
 };

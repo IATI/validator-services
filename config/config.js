0c1e047002cdcc8013ebe7c9d785ca5057ff7688
Alex Lydiate <alexl@devinit.org>
2021-04-27 10:37:47 +0100

    Exisiting reports route complete

--- config/config.js
+++ config/config.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 
 module.exports = {
-    APP_NAME: 'IATI App Name Here',
+    APP_NAME: 'Validator Services',
     VERSION: process.env.npm_package_version,
     NODE_ENV: process.env.NODE_ENV,
     APPINSIGHTS_INSTRUMENTATIONKEY: process.env.APPINSIGHTS_INSTRUMENTATIONKEY,
     NS_PER_SEC: 1e9,
+    PGCONFIG: {
+        host: process.env.PGHOST,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        port: process.env.PGPORT,
+        ssl: process.env.PGSSL,
+    },
 };

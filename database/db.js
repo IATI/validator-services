d246851ce6a35b6aacd3150807e34d908eb79107
Alex Lydiate <alexl@devinit.org>
2021-04-30 09:16:17 +0100

    added publisher and publisher-documents routes

--- database/db.js
+++ database/db.js
@@ -17,9 +17,8 @@ module.exports = {
 
         if (result.rows.length > 0) {
             return result.rows[0];
-        } 
-            return null;
-        
+        }
+        return null;
     },
 
     getReportForUrl: async (url) => {
@@ -48,5 +47,43 @@ module.exports = {
             WHERE document_id = $1
         `;
         return module.exports.getFirstRow(sql, [id]);
+    },
+
+    getPublishersWithDocuments: async () => {
+        const sql = `
+            SELECT org_id, name, description, title, state, image_url, country_code, package_count, iati_id
+            FROM publisher
+            WHERE package_count > 0
+        `;
+        return module.exports.query(sql);
+    },
+
+    getAllPublishers: async () => {
+        const sql = `
+            SELECT org_id, name, description, title, state, image_url, country_code, package_count, iati_id
+            FROM publisher
+        `;
+        return module.exports.query(sql);
+    },
+
+    getDocumentsForPublisher: async (id) => {
+        const sql = `
+        SELECT 
+            doc.id, 
+            doc.hash, 
+            doc.url, 
+            doc.first_seen, 
+            doc.downloaded,
+            doc.download_error,
+            doc.validation,
+            doc.publisher,
+            val.created as validation_created, 
+            val.valid
+        FROM document as doc
+        LEFT JOIN validation AS val ON doc.validation = val.document_hash
+        WHERE doc.publisher = $1
+        ORDER BY url ASC
+        `;
+        return module.exports.query(sql, [id]);
     },
 };

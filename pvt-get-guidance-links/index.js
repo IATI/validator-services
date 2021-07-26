87c7ddf923b819fb4377a9884b17c903db8b37a8
nosvalds <niko@devinit.org>
2021-07-26 12:21:30 +0100

    pull guidance links from GitHub directly

--- pvt-get-guidance-links/index.js
+++ pvt-get-guidance-links/index.js
@@ -1,5 +1,7 @@
 const config = require('../config/config');
 
+const { getFileCommitSha, getFileBySha } = require('../utils/utils');
+
 module.exports = async (context, req) => {
     try {
         const { version } = req.params;
@@ -18,10 +20,52 @@ module.exports = async (context, req) => {
             return;
         }
 
+        const rulesetBranch = `v${version}/validatorV2`;
+
+        console.log({
+            name: `Fetching ruleset for version: ${version}, repo: IATI-Rulesets, branch: ${rulesetBranch} `,
+            value: true,
+        });
+
+        const commitSha = await getFileCommitSha(
+            'IATI',
+            'IATI-Rulesets',
+            rulesetBranch,
+            'rulesets/standard.json'
+        );
+
+        const ruleset = await getFileBySha(
+            'IATI',
+            'IATI-Rulesets',
+            commitSha,
+            'rulesets/standard.json'
+        );
+
+        const guidanceLinks = Object.keys(ruleset).reduce((acc, xpath) => {
+            Object.keys(ruleset[xpath]).forEach((ruleType) => {
+                ruleset[xpath][ruleType].cases.forEach((oneCase) => {
+                    if (ruleType !== 'loop') {
+                        acc[oneCase.ruleInfo.id] = oneCase.ruleInfo.link;
+                    } else {
+                        Object.keys(oneCase.do).forEach((ifThen) => {
+                            oneCase.do[ifThen].cases.forEach((aCase) => {
+                                acc[aCase.ruleInfo.id] = aCase.ruleInfo.link;
+                            });
+                        });
+                    }
+                });
+            });
+            return acc;
+        }, {});
+
         context.res = {
             status: 200,
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(version),
+            body: JSON.stringify({
+                version,
+                commitSha,
+                content: guidanceLinks,
+            }),
         };
         return;
     } catch (e) {

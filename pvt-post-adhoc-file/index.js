2b681a4d778149a4791fd0f0326cff4d7d23e017
Alex Lydiate <alexl@devinit.org>
2021-08-09 08:56:56 +0100

    Extra checks and a bit of temporary debug logging

--- pvt-post-adhoc-file/index.js
+++ pvt-post-adhoc-file/index.js
@@ -1,10 +1,10 @@
 const multipart = require('parse-multipart');
 const db = require('../database/db');
 
-function endWithBadResponse(context, message = 'Bad Request') {
+function endWithBadResponse(context, message = 'Bad Request', status = 400) {
     context.log.error(message);
     context.bindings.response = {
-        status: 400,
+        status,
         body: message,
     };
     context.done();
@@ -24,16 +24,26 @@ module.exports = async (context, req) => {
             return endWithBadResponse(context, `No sessionId apparent`);
         }
 
+        console.log(req.body);
+
         const bodyBuffer = Buffer.from(req.body);
 
         const boundary = multipart.getBoundary(req.headers['content-type']);
         const parts = multipart.Parse(bodyBuffer, boundary);
+
+        if (parts.length < 1) {
+            return endWithBadResponse(
+                context,
+                `There would appear to be no file in the request body.`
+            );
+        }
 
         context.bindings.storage = parts[0].data;
 
         await db.insertAdhocValidation(req.query.sessionId, req.query.filename);
         return context.done();
     } catch (err) {
+        // MONDAY log more error
         context.log.error(err.message);
         throw err;
     }
